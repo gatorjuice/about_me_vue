275d377b36938dab4407902368f23370b1210940
James Gates <gatorjuice@gmail.com>
2021-11-09 06:58:03 -0600

    added table to view and allowed for sorting

--- src/components/RepoTrackerChart.vue
+++ src/components/RepoTrackerChart.vue
@@ -11,15 +11,8 @@ export default {
       type: Array,
       required: false,
     },
-    category: {
-      type: String,
-      required: true,
-    },
   },
   watch: {
-    category() {
-      this.resetChart();
-    },
     repos() {
       this.resetChart();
     },
@@ -34,48 +27,37 @@ export default {
       }
       this.createChart();
     },
-    filteredRepos() {
-      return [...this.repos].filter((repo) => {
-        if (this.category !== "all") {
-          return repo.category == this.category;
-        } else {
-          return true;
-        }
-      });
-    },
     createChart() {
       this.chart = new Chart(
         document.getElementById("repoTrackerChart").getContext("2d"),
         {
           type: "bar",
           data: {
-            labels: this.filteredRepos().map((repo) => repo.name),
+            labels: this.repos.map((repo) => repo.name),
             datasets: [
               {
                 label: "Forks",
                 backgroundColor: "rgba(255, 131, 96, 0.3)",
                 borderColor: "rgba(255, 131, 96, 0.8)",
-                data: this.filteredRepos().map((repo) => repo.forks_count),
+                data: this.repos.map((repo) => repo.forks_count),
               },
               {
                 label: "Watchers",
                 backgroundColor: "rgba(89, 201, 165, 0.3)",
                 borderColor: "rgba(89, 201, 165, 0.8)",
-                data: this.filteredRepos().map((repo) => repo.watchers_count),
+                data: this.repos.map((repo) => repo.watchers_count),
               },
               {
                 label: "Stargazers",
                 backgroundColor: "rgba(27, 73, 101, 0.3)",
                 borderColor: "rgba(27, 73, 101, 0.8)",
-                data: this.filteredRepos().map((repo) => repo.stargazers_count),
+                data: this.repos.map((repo) => repo.stargazers_count),
               },
               {
                 label: "Popularity",
                 backgroundColor: "rgba(118, 153, 212, 0.3)",
                 borderColor: "rgba(118, 153, 212, 0.8)",
-                data: this.filteredRepos().map(
-                  (repo) => repo.popularity_rating
-                ),
+                data: this.repos.map((repo) => repo.popularity_rating),
               },
             ],
           },
---
+++ src/components/RepoTrackerTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="table table-success table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Project</th>
+        <th scope="col" @click="$emit('changeOrder', 'forks_count')">Forks</th>
+        <th scope="col" @click="$emit('changeOrder', 'stargazers_count')">
+          Stargazers
+        </th>
+        <th scope="col" @click="$emit('changeOrder', 'watchers_count')">
+          Watchers
+        </th>
+        <th scope="col" @click="$emit('changeOrder', 'popularity_rating')">
+          Popularity
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="repo in repos" :key="repo.id" :data-test="`repo${repo.id}`">
+        <td>{{ repo.name }}</td>
+        <td>{{ repo.forks_count }}</td>
+        <td>{{ repo.stargazers_count }}</td>
+        <td>{{ repo.watchers_count }}</td>
+        <td>{{ repo.popularity_rating }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: "RepoTrackerTable",
+  props: {
+    repos: {
+      type: Array,
+    },
+  },
+};
+</script>
+<style scoped></style>
