f23cac6c3a610a25c825af8969430e28e1331f9b
James Gates <gatorjuice@gmail.com>
2021-11-13 13:11:33 -0600

    basic jest configuration for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  preset: "@vue/cli-plugin-unit-jest",
   transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    "^.+\\.vue$": "vue-jest",
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+};
