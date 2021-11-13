b4d0ea41aa86ff104607ae4c2b696f14be556ba9
James Gates <gatorjuice@gmail.com>
2021-11-13 16:03:53 -0600

    completed actions tests

---
+++ tests/unit/store/actions.spec.js
@@ -0,0 +1,281 @@
+import actions from "@/store/actions";
+import httpService from "@/services/HttpService.js";
+
+jest.mock("@/services/HttpService.js", () => ({
+  get: jest.fn(),
+  post: jest.fn(),
+  delete: jest.fn(),
+}));
+
+describe("actions", () => {
+  let commit;
+
+  beforeEach(() => (commit = jest.fn()));
+
+  describe("initialize", () => {
+    test("initialize calls commit with INITIALIZE", () => {
+      actions.initialize({ commit });
+
+      expect(commit).toHaveBeenCalledWith("INITIALIZE");
+    });
+  });
+
+  describe("toggleApiVisualizer", () => {
+    test("toggleApiVisualizer calls commit with TOGGLE_API_VISUALIZER", () => {
+      actions.toggleApiVisualizer({ commit });
+
+      expect(commit).toHaveBeenCalledWith("TOGGLE_API_VISUALIZER");
+    });
+  });
+
+  describe("loadBooks", () => {
+    beforeEach(() => {
+      httpService.get.mockImplementation((_, callback) =>
+        callback(200, [{ title: "Go Blue" }])
+      );
+      actions.loadBooks({ commit });
+    });
+
+    test("loadBooks calls commit with START_LOADING", () => {
+      expect(commit).toHaveBeenCalledWith("START_LOADING");
+    });
+    test("loadBooks calls commit with ADD_API_REQUEST", () => {
+      expect(commit).toHaveBeenCalledWith("ADD_API_REQUEST", {
+        response: { data: [{ title: "Go Blue" }], status: 200 },
+        url: "GET /api/v1/books",
+      });
+    });
+    test("loadBooks calls commit with SET_BOOKS", () => {
+      expect(commit).toHaveBeenCalledWith("SET_BOOKS", [{ title: "Go Blue" }]);
+    });
+    test("loadBooks calls commit with STOP_LOADING", () => {
+      expect(commit).toHaveBeenCalledWith("STOP_LOADING");
+    });
+  });
+
+  describe("clearMovies", () => {
+    test("clearMovies calls commit with SET_MOVIES", () => {
+      actions.clearMovies({ commit });
+
+      expect(commit).toHaveBeenCalledWith("SET_MOVIES", []);
+    });
+  });
+
+  describe("clearRepos", () => {
+    test("initialize calls commit with SET_REPOS", () => {
+      actions.clearRepos({ commit });
+
+      expect(commit).toHaveBeenCalledWith("SET_REPOS", []);
+    });
+  });
+
+  describe("loadMovies", () => {
+    beforeEach(() => {
+      httpService.get.mockImplementation((_, callback) =>
+        callback(200, { data: [] })
+      );
+      actions.loadMovies({ commit }, "the burbs");
+    });
+
+    test("loadMovies calls commit with START_LOADING", () => {
+      expect(commit).toHaveBeenCalledWith("START_LOADING");
+    });
+    test("loadMovies calls commit with ADD_API_REQUEST", () => {
+      expect(commit).toHaveBeenCalledWith("ADD_API_REQUEST", {
+        response: { data: { data: [] }, status: 200 },
+        url: "GET /api/v1/movies?title=the burbs",
+      });
+    });
+    test("loadMovies calls commit with SET_MOVIES", () => {
+      expect(commit).toHaveBeenCalledWith("SET_MOVIES", []);
+    });
+    test("loadMovies calls commit with STOP_LOADING", () => {
+      expect(commit).toHaveBeenCalledWith("STOP_LOADING");
+    });
+  });
+
+  describe("loadRepos", () => {
+    const repos = [
+      {
+        id: 1,
+        name: "vue",
+        full_name: "vuejs/vue",
+        category: "javascript_framework",
+        forks_count: 30644,
+        stargazers_count: 190269,
+        watchers_count: 190269,
+        popularity_rating: 177157,
+      },
+      {
+        id: 2,
+        name: "react",
+        full_name: "facebook/react",
+        category: "javascript_framework",
+        forks_count: 35903,
+        stargazers_count: 177616,
+        watchers_count: 177616,
+        popularity_rating: 168295,
+      },
+    ];
+
+    beforeEach(() => {
+      httpService.get.mockImplementation((_, callback) =>
+        callback(200, {
+          data: repos,
+        })
+      );
+      actions.loadRepos({ commit }, "the burbs");
+    });
+
+    test("loadRepos calls commit with START_LOADING", () => {
+      expect(commit).toHaveBeenCalledWith("START_LOADING");
+    });
+    test("loadRepos calls commit with ADD_API_REQUEST", () => {
+      expect(commit).toHaveBeenCalledWith("ADD_API_REQUEST", {
+        response: {
+          data: {
+            data: repos,
+          },
+          status: 200,
+        },
+        url: "GET /api/v1/github_repos",
+      });
+    });
+    test("loadRepos calls commit with SET_CATEGORIES", () => {
+      expect(commit).toHaveBeenCalledWith("SET_CATEGORIES", [
+        "javascript_framework",
+      ]);
+    });
+    test("loadRepos calls commit with SET_REPOS", () => {
+      expect(commit).toHaveBeenCalledWith("SET_REPOS", repos);
+    });
+    test("loadRepos calls commit with STOP_LOADING", () => {
+      expect(commit).toHaveBeenCalledWith("STOP_LOADING");
+    });
+  });
+
+  describe("login", () => {
+    beforeEach(() => {
+      httpService.post.mockImplementation((path, payload, callback) =>
+        callback(200, {
+          data: {
+            user: {
+              username: "demo_user@test.com",
+              age: 30,
+              favorites: [],
+            },
+            token: "imatoken",
+          },
+          errors: [],
+        })
+      );
+      actions.login({ commit }, { username: "test", password: "password" });
+    });
+
+    test("login calls commit with ADD_API_REQUEST", () => {
+      expect(commit).toHaveBeenCalledWith("ADD_API_REQUEST", {
+        response: {
+          data: {
+            data: {
+              user: {
+                username: "demo_user@test.com",
+                age: 30,
+                favorites: [],
+              },
+              token: "imatoken",
+            },
+            errors: [],
+          },
+          status: 200,
+        },
+        url: "POST /api/v1/login",
+      });
+    });
+    test("login calls commit with ADD_TOKEN", () => {
+      expect(commit).toHaveBeenCalledWith("SET_TOKEN", "imatoken");
+    });
+  });
+
+  describe("removeFavoriteBook", () => {
+    beforeEach(() => {
+      httpService.delete.mockImplementation((_, callback) =>
+        callback(200, {
+          data: {
+            id: 12,
+            user_id: 3,
+            book_id: 2,
+          },
+          errors: [],
+        })
+      );
+      actions.removeFavoriteBook({ commit }, { id: 1 });
+    });
+
+    test("removeFavoriteBook calls commit with ADD_API_REQUEST", () => {
+      expect(commit).toHaveBeenCalledWith("ADD_API_REQUEST", {
+        response: {
+          data: {
+            data: {
+              book_id: 2,
+              id: 12,
+              user_id: 3,
+            },
+            errors: [],
+          },
+          status: 200,
+        },
+        url: "DELETE /api/v1/user_books/1",
+      });
+    });
+    test("removeFavoriteBook calls commit with DESTROY_FAVORITE_BOOK", () => {
+      expect(commit).toHaveBeenCalledWith("DESTROY_FAVORITE_BOOK", { id: 1 });
+    });
+  });
+
+  describe("setFavoriteBook", () => {
+    beforeEach(() => {
+      httpService.delete.mockImplementation((_, callback) =>
+        callback(200, {
+          data: {
+            id: 15,
+            user_id: 3,
+            book_id: 2,
+          },
+          errors: [],
+        })
+      );
+      actions.setFavoriteBook({ commit }, { id: 1 });
+    });
+
+    test("setFavoriteBook calls commit with ADD_API_REQUEST", () => {
+      expect(commit).toHaveBeenCalledWith("ADD_API_REQUEST", {
+        response: {
+          data: {
+            data: {
+              token: "imatoken",
+              user: {
+                age: 30,
+                favorites: [],
+                username: "demo_user@test.com",
+              },
+            },
+            errors: [],
+          },
+          status: 200,
+        },
+        url: "POST /api/v1/user_books",
+      });
+    });
+    test("setFavoriteBook calls commit with CREATE_FAVORITE_BOOK", () => {
+      expect(commit).toHaveBeenCalledWith("CREATE_FAVORITE_BOOK", { id: 1 });
+    });
+  });
+
+  describe("setMessages", () => {
+    test("setMessages calls commit with SET_MESSAGES", () => {
+      actions.setMessages({ commit }, []);
+
+      expect(commit).toHaveBeenCalledWith("SET_MESSAGES", []);
+    });
+  });
+});
