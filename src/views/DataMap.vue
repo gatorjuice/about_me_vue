35ae28ae1fa90f327670c7e274eea9e3cac640d9
Jamie <11019755+gatorjuice@users.noreply.github.com>
2021-11-18 10:03:27 -0500

    attempting forced refresh with key (#45)

--- src/views/DataMap.vue
+++ src/views/DataMap.vue
@@ -5,6 +5,7 @@
     it myself. Nothing to really see here yet other than the map showing up.
   </p>
   <DataMapContainer
+    :key="dataMapKey"
     :fills="{
       HIGH: '#afafaf',
       LOW: '#123456',
@@ -17,12 +18,19 @@
 </template>
 
 <script>
+import { v4 as uuidv4 } from "uuid";
 import DataMapContainer from "@/components/DataMapContainer.vue";
 
 export default {
   name: "Movies",
   components: {
     DataMapContainer,
+  },
+  computed: {
+    // This should force a new key to be made for the map, causing refresh.
+    dataMapKey() {
+      return uuidv4();
+    },
   },
 };
 </script>
